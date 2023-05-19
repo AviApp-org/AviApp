2be3762b8df1cdaa2ecc832560ab80f5143996be
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-05-18 22:57:22 -0300

    classe aviario

---
+++ src/backend/api/src/main/java/br/com/aviapp/api/models/Aviario.java
@@ -0,0 +1,11 @@
+package br.com.aviapp.api.models;
+
+public class Aviario {
+  Integer quantidadeDeAvesMacho;
+  Integer quantidadeDeAvesFemea;
+  Integer quantidadeDeAvesMachoMortas;
+  Integer quantidadeDeAvesFemeaMortas;
+  Ovos[] ovos;
+
+  public Aviario() {}
+}
