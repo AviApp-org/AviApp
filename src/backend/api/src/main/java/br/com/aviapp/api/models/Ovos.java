ced90e512caf0ea037853dcbae548e25063832be
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-05-18 22:59:03 -0300

    classe ovos

---
+++ src/backend/api/src/main/java/br/com/aviapp/api/models/Ovos.java
@@ -0,0 +1,11 @@
+package br.com.aviapp.api.models;
+
+import br.com.aviapp.api.enums.EnumOvos;
+
+public class Ovos {
+  EnumOvos tipoDoOvo;
+  Integer quantidade;
+  Float valorTotal;
+  
+  public Ovos() {}
+}
