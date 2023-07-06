3794cb3de41cfd743619e9bd0c2ba085a9ac50cc
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-07-06 12:52:55 -0300

    feat: interface IControleDeAves

---
+++ src/backend/api/src/main/java/br/com/aviapp/api/interfaces/IControleDeAves.java
@@ -0,0 +1,13 @@
+package br.com.aviapp.api.interfaces;
+
+public interface IControleDeAves {
+  public void calcularTotalDeAves();
+
+  public void calcularQuantidadeDeGalos();
+
+  public void calcularQuantidadeDeGalosMortos();
+
+  public void calcularQuantidadeDeGalinhas();
+
+  public void calcularQuantidadeDeGalinhasMortas();
+}
