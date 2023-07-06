68c55028ddd02e30f28a35b13cb493b3b73a03b0
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-07-06 13:08:47 -0300

    feat: classe ControleDeAves

---
+++ src/backend/api/src/main/java/br/com/aviapp/api/controllers/ControleDeAves.java
@@ -0,0 +1,37 @@
+package br.com.aviapp.api.controllers;
+
+import br.com.aviapp.api.interfaces.IControleDeAves;
+
+public class ControleDeAves implements IControleDeAves{
+
+  @Override
+  public void calcularTotalDeAves() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularTotalDeAves'");
+  }
+
+  @Override
+  public void calcularQuantidadeDeGalos() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularQuantidadeDeGalos'");
+  }
+
+  @Override
+  public void calcularQuantidadeDeGalosMortos() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularQuantidadeDeGalosMortos'");
+  }
+
+  @Override
+  public void calcularQuantidadeDeGalinhas() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularQuantidadeDeGalinhas'");
+  }
+
+  @Override
+  public void calcularQuantidadeDeGalinhasMortas() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularQuantidadeDeGalinhasMortas'");
+  }
+  
+}
