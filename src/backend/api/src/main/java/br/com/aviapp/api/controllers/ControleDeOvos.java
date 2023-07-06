cd9b9f0fc85a362190ecb661f29e202afbb6e24d
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-07-06 13:09:51 -0300

    feat: classe ControleDeOvos

---
+++ src/backend/api/src/main/java/br/com/aviapp/api/controllers/ControleDeOvos.java
@@ -0,0 +1,26 @@
+package br.com.aviapp.api.controllers;
+
+import br.com.aviapp.api.enums.EnumTipoDoOvo;
+import br.com.aviapp.api.interfaces.IControleDeOvos;
+
+public class ControleDeOvos implements IControleDeOvos {
+
+  @Override
+  public void calcularTotalDeOvos() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularTotalDeOvos'");
+  }
+
+  @Override
+  public void calcularQuantidadeDeOvosPorTipo(EnumTipoDoOvo ovo) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularQuantidadeDeOvosPorTipo'");
+  }
+
+  @Override
+  public void calcularPorcentagemDeOvos(EnumTipoDoOvo ovo) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'calcularPorcentagemDeOvos'");
+  }
+  
+}
